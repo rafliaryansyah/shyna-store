1735331fc4b3f71fbae7ad3af43f35ecad672ef3
rafliaryansyah <raflyaryansha86@gmail.com>
2020-05-19 07:47:30 +0700

    slicing header, slider & banner

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <HeaderVyna />
+    <HeroVyna />
+    <BannerVyna /> 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderVyna from '@/components/HeaderVyna.vue';
+import HeroVyna from '@/components/HeroVyna.vue';
+import BannerVyna from '@/components/BannerVyna.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderVyna,
+    HeroVyna,
+    BannerVyna
+  }
+};
+</script>
