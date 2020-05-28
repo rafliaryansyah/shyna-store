74891c4e24ba21684998f59e55d1deb3d98e6b26
rafliaryansyah <raflyaryansha86@gmail.com>
2020-05-28 12:41:48 +0700

    dinamis list data produk

--- src/components/BannerVyna.vue
+++ src/components/BannerVyna.vue
@@ -2,106 +2,65 @@
     <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12 mt-5">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
                     <carousel class="product-slider" :items="3" :nav="false" :autoplay="false"> 
-                        <div class="product-item">
+
+                        <div class="product-item" v-for="itemProduct in products" v-bind:key="itemProduct.id">
                             <div class="pi-pic">
-                                <router-link to="/product"><img src="img/mickey1.jpg" alt="" /></router-link>
+                                <router-link v-bind:to="'/product/'+itemProduct.id"><img v-bind:src="itemProduct.galleries[0].photo" alt="" /></router-link>
                                 <ul>
                                     <li class="w-icon active">
-                                        <router-link to="/product"><i class="icon_bag_alt"></i></router-link>
+                                        <router-link v-bind:to="'/product/'+itemProduct.id"><i class="icon_bag_alt"></i></router-link>
                                     </li>
-                                    <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                    <li class="quick-view"><router-link v-bind:to="'/product/'+itemProduct.id">+ Quick View</router-link></li>
                                 </ul>
                             </div>
                             <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
+                                <div class="catagory-name">{{ itemProduct.type }}</div>
                                 <a href="#">
-                                    <h5>Mickey Baggy</h5>
+                                    <h5>{{ itemProduct.name }}</h5>
                                 </a>
                                 <div class="product-price">
-                                    $14.00
+                                    ${{ itemProduct.price }}.00
                                     <span>$35.00</span>
                                 </div>
                             </div>
                         </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
+                        
                     </carousel>
                 </div>
+
+                <div class="col-lg-12" v-else>
+                <p>
+                    Product belum tersedia.    
+                </p>                
+                </div>
+    
             </div>
         </div>
     </section>
 </template>
 
 <script>
-import carousel from 'vue-owl-carousel'
+import carousel from 'vue-owl-carousel';
+import axios from "axios";
 
 export default {
     name: 'BannerVyna',
     components: {
         carousel
+    },
+    data() {
+        return {
+            products: []
+        };
+    },
+    mounted() {
+        axios
+        .get("http://learn-laravue.test/api/products")
+        .then(res => (this.products = res.data.data.data))
+        // eslint-disable-next-line no-console
+        .catch(err => console.log(err));
     }
 }
 </script>
